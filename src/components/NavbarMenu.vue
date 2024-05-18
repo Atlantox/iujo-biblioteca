83344a7472ee5ac3aa53cee1b137941094e773d7
Atlantox <atlantox7@gmail.com>
2024-05-17 21:20:39 -0400

    <fix> navbar usable

--- src/components/NavbarMenu.vue
+++ src/components/NavbarMenu.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="container-fluid position-relative p-0">
-        <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
+        <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0 shadowed-l lb-bg-primary">
             <a href="index.html" class="navbar-brand p-0">
               <figure class="col-7">
                 <img class="w-50" src="" alt="upp logo">
@@ -15,28 +15,42 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav ms-auto py-0">
-                    <a href="index.html" class="nav-item nav-link active">Home</a>
-                    <a href="about.html" class="nav-item nav-link">About</a>
-                    <a href="service.html" class="nav-item nav-link">Services</a>
+                    <a href="index.html" class="nav-item nav-link fs-5">Home</a>
+                    <a href="about.html" class="nav-item nav-link fs-5">About</a>
+                    <a href="service.html" class="nav-item nav-link fs-5">Services</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle fs-5" data-bs-toggle="dropdown">Pages</a>
                         <div class="dropdown-menu m-0">
-                            <a href="appointment.html" class="dropdown-item">Appointment</a>
-                            <a href="feature.html" class="dropdown-item">Features</a>
-                            <a href="blog.html" class="dropdown-item">Our Blog</a>
-                            <a href="team.html" class="dropdown-item">Our Team</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="404.html" class="dropdown-item">404 Page</a>
+                            <a href="appointment.html" class="dropdown-item fs-6">Appointment</a>
+                            <a href="feature.html" class="dropdown-item fs-6">Features</a>
+                            <a href="blog.html" class="dropdown-item fs-6">Our Blog</a>
+                            <a href="team.html" class="dropdown-item fs-6">Our Team</a>
+                            <a href="testimonial.html" class="dropdown-item fs-6">Testimonial</a>
                         </div>
                     </div>
-                    <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+                    <a href="contact.html" class="nav-item nav-link fs-5">Contact Us</a>
                 </div>
-                <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
             </div>
         </nav>
       </div>
 </template>
 
 <style scoped>
-
+.navbar .dropdown-toggle::after {
+  content: "\f282";
+  display: inline-block;
+  font-family: bootstrap-icons !important;
+  font-size: var(--copyright-font-size);
+  font-style: normal;
+  font-weight: normal !important;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  vertical-align: -.125em;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+  left: 2px;
+  border: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import "@/assets/common.scss"
 
 //import "bootstrap/dist/css/bootstrap.min.css";
 //import "bootstrap"
-import "bootstrap-icons/font/bootstrap-icons.css"
+//import "bootstrap-icons/font/bootstrap-icons.css"
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
