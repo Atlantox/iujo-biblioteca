4ccb1795308eeea0e3fe03278c1ba6a30a30c28a
Atlantox7 <atlantox7@gmail.com>
2024-05-20 08:28:17 -0400

    <fix> adding real urls

--- src/stores/config.js
+++ src/stores/config.js
@@ -1,6 +1,7 @@
 class ApiConfig{
     constructor(){
-        this.base_url ='http://localhost:5000'
+        this.base_url ='http://172.31.2.248:5000'
+        //this.base_url ='http://localhost:5000'
     }
 }
 
